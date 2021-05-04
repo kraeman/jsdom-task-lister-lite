08fa5e893976a7d1082cd4b701cac5aae84512a0
Krae <kraewind@gmail.com>
2021-05-04 16:01:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+
+  function stop(event) {
+    event.preventDefault();
+  }
+  function getValue() {
+    return document.querySelector('#new-task-description').value
+  }
+
+  function createMenuItem() {
+    let li = document.createElement('li');
+    li.textContent = getValue();
+    return li;
+}
+
+  function addMenuItem() {
+    document.querySelector("#tasks").appendChild(createMenuItem())
+  }
+
+function all(event) {
+  stop(event)
+  addMenuItem()
+  
+  
+}
+
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', all)
 });
